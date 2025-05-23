bce4746cfbdf7357be2f29e2e2d1cfb1f8c67696
Ritesh Mishra <94739978+rm7054613349@users.noreply.github.com>
2025-05-23 15:38:24 +0530

    Update SignUp.jsx

--- src/Pages/Auth/SignUp.jsx
+++ src/Pages/Auth/SignUp.jsx
@@ -67,7 +67,7 @@ const SignUp = () => {
       if (token) {
         localStorage.setItem("token", token);
         updateUser(user);
-        navigate("/dashboard");
+        navigate("/login");
       }
 
     } catch (error) {
