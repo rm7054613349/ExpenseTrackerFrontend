77c69a810199958c619431c086389895786ee316
rm7054613349 <wwwritesh72660@gmail.com>
2025-05-23 09:41:28 +0530

    update

--- src/Utils/apiPaths.js
+++ src/Utils/apiPaths.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://expencetrackerbackend-1.onrender.com"
- export const BASE_URL="http://localhost:8000"
+export const BASE_URL = "https://expencetrackerbackend-1.onrender.com"
+//  export const BASE_URL="http://localhost:8000"
 
 export const API_PATHS = {
     AUTH: {
